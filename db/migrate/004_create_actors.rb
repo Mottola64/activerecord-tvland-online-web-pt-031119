71d83286852a0db4d522d9a4d2e8537afd1422e6
Michael Mottola <mottola64@gmail.com>
2019-06-17 00:53:55 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,9 +1,9 @@
 
-class CreateActors < ActiveRecord::Migration[4.2]
-def change
-create_table :actors do |t|
-  t.string :first_name
-  t.string :last_name
-end
-end
+class CreateCharacters < ActiveRecord::Migration[4.2]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :show_id
+    end
+  end
 end
